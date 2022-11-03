b24b82a7f8724b9035c83ce5863e98ca8186dc0b
PEDRO BIHAN <pbihan@uc.cl>
2022-11-03 13:44:02 -0300

    grafico y consulta por ciudades de forma estatico funcionando, la tabla de ciudades no tiene estilo si, tuve que comentar los headers pq no se hacia deploy en netlify por eslint unused variables

--- src/components/successInfo.jsx
+++ src/components/successInfo.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
 import '../App.css';
 import { PieChart } from 'react-minimal-pie-chart';
 
