82c9d3bf6b65261c479ec9417311aa765bc913ee
PEDRO BIHAN <pbihan@uc.cl>
2022-11-03 12:21:19 -0300

    grfico estatico de success en componente para hacer el fetch comentado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import SuccessInfo from './components/successInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <SuccessInfo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
